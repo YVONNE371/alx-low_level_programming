9e4036cb3ef8d975493068cf23ee376363afe46a
Yvonne Awuor Awuor <yvoneawuor6@gmail.com>
2023-02-16 10:03:39 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - prints "programming is like building a multilingual puzzle, followed by a new line. "
- * return 0.
- *
+ * main - prints "programming is like building a multilingual puzzle"
+ * followed by a new line. 
  * Return: Always 0 (Success)
  */
 int main(void)
