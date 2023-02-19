364b4e655c82351ce0b507ccad930c1f66788e07
Yvonne Awuor Awuor <yvoneawuor6@gmail.com>
2023-02-19 02:21:16 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Prints all possible combinations of single-digit number
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numStart;
+
+	for (numStart = 0; numStart <= 9; numStart++)
+	{
+		putchar(numStart + '0');
+		if (numStart == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
