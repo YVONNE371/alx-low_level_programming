02cd245be9c5904b3393715569fca7bea52ee325
Yvonne Awuor Awuor <yvoneawuor6@gmail.com>
2023-02-19 02:28:18 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * 00 01 - 98 99
+ * New line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int leftNumber;
+	int rightNumber;
+
+	for (leftNumber = 0; leftNumber <= 98; leftNumber++)
+	{
+		for (rightNumber = leftNumber + 1; rightNumber <= 99; rightNumber++)
+		{
+			putchar((leftNumber / 10) + '0');
+			putchar((leftNumber % 10) + '0');
+			putchar(' ');
+			putchar((rightNumber / 10) + '0');
+			putchar((rightNumber % 10) + '0');
+			if (leftNumber == 98 && rightNumber == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
