9c474a0ac0f60a3a5b4db948332cf09c8f28b721
Yvonne Awuor Awuor <yvoneawuor6@gmail.com>
2023-02-19 02:18:55 -0800

    8 alphabets

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * declaration of myStart
+ * start of number for loop and initialization of myStart
+ * printing 0-9
+ * printing a-f
+ * New line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int myNumStart;
+	char myCharStart;
+
+	for (myNumStart = 0; myNumStart <= 9; myNumStart++)
+		putchar(myNumStart + '0');
+	for (myCharStart = 'a'; myCharStart <= 'f'; myCharStart++)
+		putchar(myCharStart);
+	putchar('\n');
+	return (0);
+}
