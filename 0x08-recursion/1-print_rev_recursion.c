5684b7ab08344c9e5f1bd3fe1d5079c8b1b352e1
Yvonne Awuor Awuor <yvoneawuor6@gmail.com>
2023-03-08 09:26:36 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Print a string in reverse
+ * @s: the string to reverse
+ *
+ * Return: Nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\n')
+	{
+		return;
+	}
+
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(char *s);
+}
