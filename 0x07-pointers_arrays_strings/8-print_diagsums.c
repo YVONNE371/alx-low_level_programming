b6731a26f7888d34313e6767dbe54133fa6542f1
Yvonne Awuor Awuor <yvoneawuor6@gmail.com>
2023-03-08 04:34:04 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,20 +1,22 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_diagsums - Entry point
- * @a: input
- * @size: input
- * Return: Always 0 (success)
+ * print_chessboard - Prints a chessboard
+ * @a: array for chess board
+ *
+ * Return: void
  */
-void print_diagsums(int *a, int size)
-{ 
-	int i, n, sum1 = 0, sum2 = 0;
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
 
-	for (i = 0; i <= (size * size); i = i + size + 1)
-		sum1 = sum1 + a[i];
+	for (x = 0; x < 8; x++)
+	{
+		for (y = 0; y < 8; y++)
+		{
+			_putchar(a[x][y]);
+		}
 
-	for (n = size - 1; n <= (size * size) -size; n = n + size - 1)
-		sum2 = sum2 + a[n];
-	printf("%d, %d\n", sum1, sum2);
+		_putchar('\n');
+	}
 }
