92df352485859d5e0ac69c0c137ea677a6b4272d
Yvonne Awuor Awuor <yvoneawuor6@gmail.com>
2023-04-04 13:12:57 -0700

    11. Reverse list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - Reverse an LL
+ * @head: head list pointer
+ *
+ * Return: first node pointer
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *previous = NULL;
+	listint_t *current = NULL;
+
+	if (head)
+	{
+		while (*head != NULL)
+		{
+			current = *head;
+			*head = (*head)->next;
+			current->next = previous;
+			previous = current;
+		}
+
+		*head = previous;
+		return (*head);
+	}
+
+	return (NULL);
+}
