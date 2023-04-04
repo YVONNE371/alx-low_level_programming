f4186c38c9d01a7e0177e382480c56235d3189d3
Yvonne Awuor Awuor <yvoneawuor6@gmail.com>
2023-04-04 09:24:42 -0700

    8. Sum list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - sum of all the data->n
+ * @head: List head pointer
+ *
+ * Return: sum of n or 0
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		while (head != NULL)
+		{
+			sum = sum + head->n;
+			head = head->next;
+		}
+	}
+	return (sum);
+}
