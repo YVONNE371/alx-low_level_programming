5ebc3b3de17776bd9f15812282326d46b0e6dcd6
Yvonne Awuor Awuor <yvoneawuor6@gmail.com>
2023-04-04 09:03:42 -0700

    6. Pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes head node
+ * @head: head pointer
+ *
+ * Return: node's data n or 0
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int my_n = 0;
+
+	if (*head == NULL)
+		return (my_n);
+
+	my_n = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (my_n);
+}
